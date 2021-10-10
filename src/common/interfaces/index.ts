ebc328cd97ea8227269d4d03830668bbcdb4e1de
wils446 <wilsonlimmmm@gmail.com>
2021-10-10 11:30:47 +0700

    add index.ts for import interface

---
+++ src/common/interfaces/index.ts
@@ -0,0 +1,5 @@
+import { DataInterface } from "./DataInterface";
+import { Data, Technology, YearsData } from "./DataTyping";
+import { CategoryI } from "./VisibleCategoryInterface";
+
+export type { DataInterface, Data, Technology, YearsData, CategoryI };
