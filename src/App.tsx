fca267410396802282a2b8b896cab4cd6d466824
wils446 <wilsonlimmmm@gmail.com>
2022-04-23 02:22:28 +0700

    add simple early page

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,27 @@
 import "./App.css";
+import { useState } from "react";
+import { Technology } from "./common/interface/DataTyping";
+import surveyDataFile from "./data/survey.json";
 
 function App() {
-    return <div className="App"></div>;
+    const surveyData = new Map<string, Technology>();
+    surveyDataFile.forEach((d) => surveyData.set(d[0] as string, d[1] as Technology));
+
+    const [techList] = useState([...surveyData.keys()]);
+
+    return (
+        <div className="App">
+            <header className="app-header">
+                <h1 className="text-5xl font-semibold tracking-wider font-montserrat mb-8">TechSight</h1>
+                <input type="text" list="languages" autoComplete="off" />
+                <datalist id="languages">
+                    {techList.map((t, index) => {
+                        return <option className="w-full" key={index} value={t} />;
+                    })}
+                </datalist>
+            </header>
+        </div>
+    );
 }
 
 export default App;
