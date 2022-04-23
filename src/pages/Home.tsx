5b52a8c2c82fa3a294aadee8d957195e1071f46e
wils446 <wilsonlimmmm@gmail.com>
2022-04-23 15:45:38 +0700

    change input into multiple select

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,24 +1,47 @@
 import { Technology } from "../common/interface/DataTyping";
 import surveyDataFile from "../data/survey.json";
-import { useState } from "react";
+import Select from "react-select";
+import { useEffect, useState } from "react";
+import { LineChart } from "../components";
 
 function Home() {
     const surveyData = new Map<string, Technology>();
     surveyDataFile.forEach((d) => surveyData.set(d[0] as string, d[1] as Technology));
 
     const [techList] = useState([...surveyData.keys()]);
-    const [input, setInput] = useState("");
+    const [selectedTech, setSelectedTech] = useState<Technology[]>([]);
+    const [input, setInput] = useState<string[]>([]);
+
+    const getTechOptions = () => {
+        return techList.map((t) => ({ value: t, label: t }));
+    };
+
+    useEffect(() => {
+        input.forEach((t) => {
+            if (surveyData.has(t)) {
+                setSelectedTech((prev) => [...prev, surveyData.get(t) as Technology]);
+            }
+
+            if (selectedTech.length > input.length) {
+                setSelectedTech((prev) => prev.filter((tech) => input.includes(tech.name)));
+            }
+        });
+        // eslint-disable-next-line react-hooks/exhaustive-deps
+    }, [input]);
 
     return (
         <div className="App">
             <header className="app-header">
                 <h1 className="text-5xl font-semibold tracking-wider font-montserrat mb-8">TechSight</h1>
-                <input type="text" list="languages" onChange={(e) => setInput(e.target.value)} autoComplete="off" />
-                <datalist id="languages">
-                    {techList.map((t, index) => {
-                        return <option className="w-full" key={index} value={t} />;
-                    })}
-                </datalist>
+                <Select
+                    className="text-left"
+                    isMulti
+                    options={getTechOptions()}
+                    onChange={(e) => {
+                        setInput(e.map((t) => t.value));
+                    }}
+                    placeholder="Select Technology..."
+                />
             </header>
         </div>
     );
