7ae360e5390192372f7be13a93c72284dbfe994d
wils446 <wilsonlimmmm@gmail.com>
2022-04-23 12:48:47 +0700

    setup router

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,12 @@
 import "./App.css";
-import { useState } from "react";
-import { Technology } from "./common/interface/DataTyping";
-import surveyDataFile from "./data/survey.json";
+import { BrowserRouter } from "react-router-dom";
+import { Routes } from "./router";
 
 function App() {
-    const surveyData = new Map<string, Technology>();
-    surveyDataFile.forEach((d) => surveyData.set(d[0] as string, d[1] as Technology));
-
-    const [techList] = useState([...surveyData.keys()]);
-
     return (
-        <div className="App">
-            <header className="app-header">
-                <h1 className="text-5xl font-semibold tracking-wider font-montserrat mb-8">TechSight</h1>
-                <input type="text" list="languages" autoComplete="off" />
-                <datalist id="languages">
-                    {techList.map((t, index) => {
-                        return <option className="w-full" key={index} value={t} />;
-                    })}
-                </datalist>
-            </header>
-        </div>
+        <BrowserRouter>
+            <Routes />
+        </BrowserRouter>
     );
 }
 
---
+++ src/pages/Home.tsx
@@ -0,0 +1,26 @@
+import { Technology } from "../common/interface/DataTyping";
+import surveyDataFile from "../data/survey.json";
+import { useState } from "react";
+
+function Home() {
+    const surveyData = new Map<string, Technology>();
+    surveyDataFile.forEach((d) => surveyData.set(d[0] as string, d[1] as Technology));
+
+    const [techList] = useState([...surveyData.keys()]);
+
+    return (
+        <div className="App">
+            <header className="app-header">
+                <h1 className="text-5xl font-semibold tracking-wider font-montserrat mb-8">TechSight</h1>
+                <input type="text" list="languages" autoComplete="off" />
+                <datalist id="languages">
+                    {techList.map((t, index) => {
+                        return <option className="w-full" key={index} value={t} />;
+                    })}
+                </datalist>
+            </header>
+        </div>
+    );
+}
+
+export default Home;
